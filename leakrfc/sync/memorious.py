15399ddbd47d14b0b47201a705809426416031be
Simon Wörpel <simon.woerpel@pm.me>
2024-10-09 21:04:21 +0200

    🚧 (sync/memorious) Fail on task level

--- leakrfc/sync/memorious.py
+++ leakrfc/sync/memorious.py
@@ -11,40 +11,15 @@ memorious format:
 from pathlib import Path
 from urllib.parse import urlparse
 
-from anystore.store import BaseStore, get_store
-from anystore.types import Uri
+from anystore.store import get_store
+from anystore.types import StrGenerator, Uri
 
 from leakrfc.archive import DatasetArchive
 from leakrfc.logging import get_logger
-from leakrfc.model import OriginalFile, OriginalFiles
+from leakrfc.model import OriginalFile
 from leakrfc.worker import DatasetWorker
 
 log = get_logger(__name__)
-
-
-def iter_memorious(store: BaseStore, dataset: str) -> OriginalFiles:
-    for key in store.iterate_keys():
-        if key.endswith(".json"):
-            data = store.get(key, serialization_mode="json")
-            content_hash = data.pop("content_hash", None)
-            if content_hash is None:
-                log.warning(f"No content hash for `{key}`", store=store.uri)
-            elif data.get("_file_name") is None:
-                log.warning(f"No original file for `{key}`", store=store.uri)
-            else:
-                parsed = urlparse(data["url"])
-                p = Path(parsed.path)
-                info = store.info(data["_file_name"])
-                yield OriginalFile(
-                    key=str(p).lstrip("/"),
-                    name=p.name,
-                    path=str(p.parent).lstrip("/"),
-                    size=info.size,
-                    content_hash=content_hash,
-                    store=str(store.uri),
-                    dataset=dataset,
-                    extra=data,
-                )
 
 
 class MemoriousWorker(DatasetWorker):
@@ -52,15 +27,40 @@ class MemoriousWorker(DatasetWorker):
         super().__init__(dataset, *args, **kwargs)
         self.memorious = get_store(uri, serialization_mode="raw")
 
-    def get_tasks(self) -> OriginalFiles:
-        yield from iter_memorious(self.memorious, self.dataset.name)
+    def get_tasks(self) -> StrGenerator:
+        yield from self.memorious.iterate_keys(glob="*.json")
 
-    def handle_task(self, task: OriginalFile) -> None:
-        self.dataset.archive_file(
-            task.extra.pop("_file_name"),
-            store=self.memorious,
-            file=task,
-        )
+    def handle_task(self, task: str) -> None:
+        file = self.load_memorious(task)
+        if file is not None:
+            if not self.dataset.exists(file.key):
+                self.dataset.archive_file(
+                    file.extra.pop("_file_name"),
+                    store=self.memorious,
+                    file=file,
+                )
+
+    def load_memorious(self, key: str) -> OriginalFile | None:
+        data = self.memorious.get(key, serialization_mode="json")
+        content_hash = data.pop("content_hash", None)
+        if content_hash is None:
+            log.warning(f"No content hash for `{key}`", store=self.memorious.uri)
+        elif data.get("_file_name") is None:
+            log.warning(f"No original file for `{key}`", store=self.memorious.uri)
+        else:
+            parsed = urlparse(data["url"])
+            p = Path(parsed.path)
+            info = self.memorious.info(data["_file_name"])
+            return OriginalFile(
+                key=str(p).lstrip("/"),
+                name=p.name,
+                path=str(p.parent).lstrip("/"),
+                size=info.size,
+                content_hash=content_hash,
+                store=str(self.memorious.uri),
+                dataset=self.dataset.name,
+                extra=data,
+            )
 
     def done(self) -> None:
         self.log_info(f"Done memorious import from `{self.memorious.uri}`")
